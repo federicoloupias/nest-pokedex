b3b0ddbbf3239915ec66f6a84866dede223a506d
Federico Loupias <federicoloupias@gmail.com>
2024-06-26 23:32:59 -0300

    Preparar mongoDB y envs

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,14 +5,24 @@ import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { paginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number 
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) { }
+
+    private readonly configService: ConfigService,
+
+  ) {
+
+    this.defaultLimit = configService.get<number>('defaultLimit')
+  
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -28,7 +38,7 @@ export class PokemonService {
   }
 
   findAll( paginationDto: paginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto
     // const pokemons: Pokemon[] = this.pokemonModel.finAll();
     return this.pokemonModel.find()
     .limit( limit )
