b4fd2641192f6e13ce6e271124117157168d549e
Federico Loupias <federicoloupias@gmail.com>
2024-06-26 23:43:44 -0300

    Cambio de envs para deploy

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,4 @@
 export const EnvConfiguration = () => ({
-    enviroment: process.env.NODE_ENV || 'dev',
     mongodb: process.env.MONGODB,
     port: process.env.PORT || 3002,
     defaultLimit: +process.env.DEFAULT_LIMIT || 7,
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -3,6 +3,6 @@ import * as Joi from "joi";
 
 export const JoiValidationSchema = Joi.object({
     MONGODB: Joi.required(),
-    NODE_ENV: Joi.number().default(3005),
+    PORT: Joi.number().default(3005),
     DEFAULT_LIMIT: Joi.number().default(6),
 });
